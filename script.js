156afce3c50417c19e6b67662cf88dfd0bebc8de
MUHAMMAD HUSSAIN SHAMIM <142653978+hussainshamim16@users.noreply.github.com>
2023-11-13 14:54:15 +0500

    added same

---
+++ script.js
@@ -0,0 +1,63 @@
+// console.log('hello world')
+
+// var swiper = new Swiper(".mySwiper", {
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+  // var swiper = new Swiper(".mySwiper", {
+  //   spaceBetween: 30,
+  //   effect: "fade",
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  // });
+
+
+  // var swiper = new Swiper(".meSwiper", {
+  //   spaceBetween: 30,
+  //   centeredSlides: true,
+  //   autoplay: {
+  //     delay: 3500,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-paginatiion",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-buttoon-next",
+  //     prevEl: ".swiper-buttoon-prev",
+  //   },
+  // });
+
+
+  var swiper = new Swiper(".myySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+ 
+
+
+  // --------------------
+
